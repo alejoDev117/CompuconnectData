9ca2db8b10318550e40d0bc0fa8a5b1a852441cc
Alejandro Gómez Orjuela <102133299+alejoDev117@users.noreply.github.com>
2023-05-27 19:08:07 -0500

    cambio agendaPostgresqlDAO

--- src/co/edu/uco/compuconnect/data/dao/relational/SqlDAO.java
+++ src/co/edu/uco/compuconnect/data/dao/relational/SqlDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.util.List;
 
 import co.edu.uco.compuconnect.crosscutting.exceptions.CompuconnectDataException;
+import co.edu.uco.compuconnect.crosscutting.utils.Messages.SqlDAOMessage;
 import co.edu.uco.compuconnect.crosscutting.utils.UtilSql;
 
 public abstract class SqlDAO<E> {
@@ -20,10 +21,7 @@ public abstract class SqlDAO<E> {
 
 	private final void setConnection(final Connection connection) {
 		if(!UtilSql.connectionIsOpen(connection)) {
-			var userMessage = "Se ha presentado un error tratando de llevar a cabo la operacion deseada por favor intente de nuevo y si el problema persiste contecte al administrador del sistema";
-			var technicalMessage = "No se ha podido crear el SqlDAO debido a que la conexion no esta abierta";
-			
-			throw CompuconnectDataException.create(technicalMessage, userMessage);
+			throw CompuconnectDataException.create(SqlDAOMessage.SET_CONNECTION_EXCEPTION_TECHNICAL_MESSAGE, SqlDAOMessage.SET_CONNECTION_EXCEPTION_USER_MESSAGE);
 		}
 		
 		this.connection = connection;
--- src/co/edu/uco/compuconnect/data/dao/relational/postgresql/AgendaPostgresqlDAO.java
+++ src/co/edu/uco/compuconnect/data/dao/relational/postgresql/AgendaPostgresqlDAO.java
@@ -8,88 +8,131 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import co.edu.uco.compuconnect.crosscutting.exceptions.CompuconnectDataException;
+import co.edu.uco.compuconnect.crosscutting.utils.UtilObject;
+import co.edu.uco.compuconnect.crosscutting.utils.UtilUUID;
+import co.edu.uco.compuconnect.crosscutting.utils.Messages.AgendaPostgresqlDAOMessage;
 import co.edu.uco.compuconnect.data.dao.AgendaDAO;
+import co.edu.uco.compuconnect.data.dao.relational.SqlDAO;
 import co.edu.uco.compuconnect.entities.AgendaEntity;
 import co.edu.uco.compuconnect.entities.PeriodoFuncionamientoEntity;
 
-public final class AgendaPostgresqlDAO implements AgendaDAO {
+public final class AgendaPostgresqlDAO extends SqlDAO<AgendaEntity> implements  AgendaDAO {
 
-    private final Connection connection;
 
+   
     public AgendaPostgresqlDAO(final Connection connection) {
-        this.connection = connection;
-    }
+		super(connection);
+	}
 
-    @Override
+	@Override
     public void create(final AgendaEntity entity) {
-        String sql = "INSERT INTO agenda (identificador, periodo_funcionamiento, centro_informatica, nombre) VALUES (?, ?, ?, ?)";
+        String sqlStatement = "INSERT INTO agenda (identificador,\"periodoFuncionamiento\", \"centroInformatica\", nombre) VALUES (?, ?, ?, ?)";
 
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+        try (PreparedStatement statement = getConnection().prepareStatement(sqlStatement)) {
             statement.setObject(1, entity.getIdentificador());
-            statement.setObject(2, entity.getPeriodoFuncionamiento().toString());
-            statement.setObject(3, entity.getCentroInformatica());
+            statement.setObject(2, entity.getPeriodoFuncionamiento().getIdentificador());
+            statement.setObject(3, entity.getCentroInformatica().getIdentificador());
             statement.setString(4, entity.getNombre());
 
             statement.executeUpdate();
-        } catch (SQLException e) {
-        	//excepcion
+        } catch (final SQLException exception) {
+        	throw CompuconnectDataException.create(AgendaPostgresqlDAOMessage.CREATE_SQL_EXCEPTION_TECHNICAL_MESSAGE, AgendaPostgresqlDAOMessage.CREATE_SQL_EXCEPTION_USER_MESSAGE,exception);
         	
+        }catch(final Exception exception) {
+        	throw CompuconnectDataException.create(AgendaPostgresqlDAOMessage.CREATE_EXCEPTION_TECHNICAL_MESSAGE, AgendaPostgresqlDAOMessage.CREATE_EXCEPTION_USER_MESSAGE,exception);
         }
     }
 
     @Override
     public List<AgendaEntity> read(final AgendaEntity entity) {
-        List<AgendaEntity> agendaList = new ArrayList<>();
-        String sql = "SELECT identificador, periodo_funcionamiento, centro_informatica, nombre FROM agenda WHERE nombre = ?";
-
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setString(1, entity.getNombre());
-
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                UUID identificador = UUID.fromString(resultSet.getString("identificador"));
-                PeriodoFuncionamientoEntity periodoFuncionamiento = PeriodoFuncionamientoEntity.getDefaultObject();
-                String centroInformatica = resultSet.getString("centro_informatica");
-                String nombre = resultSet.getString("nombre");
-
-                AgendaEntity agendaEntity = AgendaEntity.getDefaultObject();
-                agendaList.add(agendaEntity);
-            }
-        } catch (SQLException e) {
-            //excepcion
-        }
-
-        return agendaList;
+    	var sqlStatement = new StringBuilder();
+    	var listParameters = new ArrayList<>();
+    	
+    	sqlStatement.append(prepareSelect());
+    	sqlStatement.append(prepareFrom());
+    	sqlStatement.append(prepareWhere(entity, listParameters));
+    	sqlStatement.append(prepareOrderBy());
+    	
+    	return null;
     }
 
     @Override
     public void delete(final AgendaEntity entity) {
-        String sql = "DELETE FROM agenda WHERE identificador = ?";
+        String sqlStatement = "DELETE FROM agenda WHERE identificador = ?";
 
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+        try (PreparedStatement statement = getConnection().prepareStatement(sqlStatement)) {
             statement.setObject(1, entity.getIdentificador());
 
             statement.executeUpdate();
-        } catch (SQLException e) {
-        	//excepcion
+        } catch (SQLException exception) {
+        	throw CompuconnectDataException.create(AgendaPostgresqlDAOMessage.DELETE_SQL_EXCEPTION_TECHNICAL_MESSAGE, AgendaPostgresqlDAOMessage.DELETE_SQL_EXCEPTION_USER_MESSAGE, exception);
+        }catch(Exception exception) {
+        	throw CompuconnectDataException.create(AgendaPostgresqlDAOMessage.DELETE_EXCEPTION_TECHNICAL_MESSAGE, AgendaPostgresqlDAOMessage.DELETE_EXCEPTION_USER_MESSAGE, exception);
         }
     }
 
     @Override
     public void uptade(AgendaEntity entity) {
-        String sql = "UPDATE agenda SET periodo_funcionamiento = ?, centro_informatica = ?, nombre = ? WHERE identificador = ?";
+        String sqlStatement = "UPDATE agenda SET periodoFuncionamiento= ?, centroInformatica= ?,nombre = ? WHERE identificador = ?";
 
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+        try (PreparedStatement statement = getConnection().prepareStatement(sqlStatement)) {
             statement.setObject(1, entity.getPeriodoFuncionamiento().toString());
             statement.setObject(2, entity.getCentroInformatica());
             statement.setString(3, entity.getNombre());
-            statement.setObject(4, entity.getIdentificador());
+            statement.setObject(4, entity.getIdentificador());	
 
             statement.executeUpdate();
-        } catch (SQLException e) {
-        	//excepcion
+        } catch (SQLException exception) {
+        	throw CompuconnectDataException.create(AgendaPostgresqlDAOMessage.UPDATE_SQL_EXCEPTION_TECHNICAL_MESSAGE, AgendaPostgresqlDAOMessage.UPDATE_SQL_EXCEPTION_USER_MESSAGE, exception);
+        }catch(Exception exception) {
+        	throw CompuconnectDataException.create(AgendaPostgresqlDAOMessage.UPDATE_EXCEPTION_TECHNICAL_MESSAGE, AgendaPostgresqlDAOMessage.UPDATE_EXCEPTION_USER_MESSAGE, exception);
         }
     }
+
+	@Override
+	protected String prepareSelect() {
+		return "SELECT identificador, periodoFuncionamiento, centroInformatica, nombre ";
+	}
+
+	@Override
+	protected String prepareFrom() {
+		return "FROM agenda ";
+	}
+
+	@Override
+	protected String prepareWhere(final AgendaEntity entity, List<Object> parameters) {
+		
+		final var where = new StringBuilder("");
+		parameters = UtilObject.getDefault(parameters, new ArrayList<>());
+		var setWhere = true;
+		
+		if(!UtilObject.isNull(entity)) {
+			
+			if(!UtilUUID.isDefault(entity.getIdentificador())) {
+				parameters.add(entity.getIdentificador());
+				where.append("WHERE identificador=? ");
+				setWhere = false;
+			}
+			if(!UtilUUID.isDefault(entity.getPeriodoFuncionamiento().getIdentificador())) {
+				parameters.add(entity.getPeriodoFuncionamiento().getIdentificador());
+				where.append(setWhere ? "WHERE" : "AND ").append("periodoFuncionamiento=? ");
+				setWhere = false;
+			}
+			if(!UtilUUID.isDefault(entity.getCentroInformatica().getIdentificador())) {
+				parameters.add(entity.getCentroInformatica().getIdentificador());
+				where.append(setWhere ? "WHERE" : "AND ").append("periodoFuncionamiento=? ");
+			}
+			
+		
+		}
+		return where.toString();
+	}
+
+	@Override
+	protected String prepareOrderBy() {
+		return "ORDER BY nombre ASC";
+	}
 
 
 }
