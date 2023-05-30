89a375a943c2ee87f9a97f9875ae616cd614e19a
Alejandro Gómez Orjuela <102133299+alejoDev117@users.noreply.github.com>
2023-05-30 14:42:09 -0500

    pruebita

--- src/co/edu/uco/compuconnect/data/dao/relational/postgresql/AgendaPostgresqlDAO.java
+++ src/co/edu/uco/compuconnect/data/dao/relational/postgresql/AgendaPostgresqlDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import co.edu.uco.compuconnect.crosscutting.exceptions.CompuconnectDataException;
 import co.edu.uco.compuconnect.crosscutting.utils.UtilObject;
@@ -13,6 +14,8 @@ import co.edu.uco.compuconnect.crosscutting.utils.Messages.AgendaPostgresqlDAOMe
 import co.edu.uco.compuconnect.data.dao.AgendaDAO;
 import co.edu.uco.compuconnect.data.dao.relational.SqlDAO;
 import co.edu.uco.compuconnect.entities.AgendaEntity;
+import co.edu.uco.compuconnect.entities.CentroInformaticaEntity;
+import co.edu.uco.compuconnect.entities.PeriodoFuncionamientoEntity;
 
 public final class AgendaPostgresqlDAO extends SqlDAO<AgendaEntity> implements  AgendaDAO {
 
@@ -51,7 +54,18 @@ public final class AgendaPostgresqlDAO extends SqlDAO<AgendaEntity> implements  
     	sqlStatement.append(prepareWhere(entity, listParameters));
     	sqlStatement.append(prepareOrderBy());
     	
-    	return null;
+    	try (var prepareStatement = getConnection().prepareStatement(sqlStatement.toString())){
+    		setParameters(prepareStatement, listParameters);
+    		return excecuteQuery(prepareStatement);
+    		
+    	}catch (CompuconnectDataException exception) {
+    		throw exception;
+    	}catch(SQLException exception) {
+    		throw CompuconnectDataException.create(AgendaPostgresqlDAOMessage.READ_SQL_EXCEPTION_TECHNICAL_MESSAGE, AgendaPostgresqlDAOMessage.READ_SQL_EXCEPTION_USER_MESSAGE, exception);
+    	}catch(Exception exception) {
+    		throw CompuconnectDataException.create(AgendaPostgresqlDAOMessage.READ_EXCEPTION_TECHNICAL_MESSAGE, AgendaPostgresqlDAOMessage.READ_EXCEPTION_USER_MESSAGE, exception);
+    	}
+    	
     }
 
     @Override
@@ -130,6 +144,47 @@ public final class AgendaPostgresqlDAO extends SqlDAO<AgendaEntity> implements  
 	protected String prepareOrderBy() {
 		return "ORDER BY nombre ASC";
 	}
-	
 
+	@Override
+	protected void setParameters(final PreparedStatement prepareStatement, final List<Object> parameters) {
+		try {
+			
+		if(!UtilObject.isNull(parameters) && !UtilObject.isNull(prepareStatement)) {
+			for(int index = 0; index < parameters.size();index++) {
+				prepareStatement.setObject(index + 1, parameters.get(index));
+				
+			}
+		}
+		}catch (SQLException exception) {
+			throw CompuconnectDataException.create(AgendaPostgresqlDAOMessage.SET_PARAMETERS_SQL_EXCEPTION_TECHNICAL_MESSAGE, AgendaPostgresqlDAOMessage.SET_PARAMETERS_SQL_EXCEPTION_USER_MESSAGE, exception);
+		}catch (Exception exception) {
+			throw CompuconnectDataException.create(AgendaPostgresqlDAOMessage.SET_PARAMETERS_EXCEPTION_TECHNICAL_MESSAGE, AgendaPostgresqlDAOMessage.SET_PARAMETERS_EXCEPTION_USER_MESSAGE, exception);
+		}
+		
+	}
+
+	@Override
+	protected List<AgendaEntity> excecuteQuery(PreparedStatement prepareStatement) {
+		
+		List<AgendaEntity> listResultSet = new ArrayList<>();		
+		
+		try(var resultSet = prepareStatement.executeQuery()){
+			
+			while(resultSet.next()) {
+				var entityTmp = new AgendaEntity(resultSet.getObject("identificador",UUID.class)
+						,resultSet.getObject("periodoFuncionamiento",PeriodoFuncionamientoEntity.class),
+						resultSet.getObject("centroInformatica",CentroInformaticaEntity.class)
+						,resultSet.getString("nombre"));
+				listResultSet.add(entityTmp);
+			}
+			
+		}catch (SQLException exception) {
+			throw CompuconnectDataException.create(AgendaPostgresqlDAOMessage.EXCECUTE_QUERY_SQL_EXCEPTION_TECHNICAL_MESSAGE, AgendaPostgresqlDAOMessage.EXCECUTE_QUERY_SQL_EXCEPTION_USER_MESSAGE, exception);
+		}catch (Exception exception) {
+			throw CompuconnectDataException.create(AgendaPostgresqlDAOMessage.EXCECUTE_QUERY_EXCEPTION_TECHNICAL_MESSAGE, AgendaPostgresqlDAOMessage.EXCECUTE_QUERY_EXCEPTION_USER_MESSAGE, exception);
+	}
+	
+		return null;
+	}
+		
 }
